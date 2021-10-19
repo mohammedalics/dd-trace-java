ead05f3629669b0869e7bc3d7cf4efad76e36aa8
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2021-10-19 15:40:08 +0200

    Send AppSec attack information together with Trace

--- internal-api/src/main/java/datadog/trace/api/Config.java
+++ internal-api/src/main/java/datadog/trace/api/Config.java
@@ -5,6 +5,7 @@ import static datadog.trace.api.ConfigDefaults.DEFAULT_AGENT_TIMEOUT;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_AGENT_WRITER_TYPE;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_ANALYTICS_SAMPLE_RATE;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_APPSEC_ENABLED;
+import static datadog.trace.api.ConfigDefaults.DEFAULT_APPSEC_REPORTING_INBAND;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_AWS_PROPAGATION_ENABLED;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_DB_CLIENT_HOST_SPLIT_BY_INSTANCE;
 import static datadog.trace.api.ConfigDefaults.DEFAULT_DOGSTATSD_START_DELAY;
@@ -70,6 +71,7 @@ import static datadog.trace.api.DDTags.SERVICE_TAG;
 import static datadog.trace.api.IdGenerationStrategy.RANDOM;
 import static datadog.trace.api.Platform.isJavaVersionAtLeast;
 import static datadog.trace.api.config.AppSecConfig.APPSEC_ENABLED;
+import static datadog.trace.api.config.AppSecConfig.APPSEC_REPORTING_INBAND;
 import static datadog.trace.api.config.AppSecConfig.APPSEC_RULES_FILE;
 import static datadog.trace.api.config.GeneralConfig.API_KEY;
 import static datadog.trace.api.config.GeneralConfig.API_KEY_FILE;
@@ -392,6 +394,7 @@ public class Config {
   private final boolean profilingUploadSummaryOn413Enabled;
 
   private final boolean appSecEnabled;
+  private final boolean appSecReportingInband;
   private final String appSecRulesFile;
 
   private final boolean awsPropagationEnabled;
@@ -814,6 +817,8 @@ public class Config {
             PROFILING_UPLOAD_SUMMARY_ON_413, DEFAULT_PROFILING_UPLOAD_SUMMARY_ON_413);
 
     appSecEnabled = configProvider.getBoolean(APPSEC_ENABLED, DEFAULT_APPSEC_ENABLED);
+    appSecReportingInband =
+        configProvider.getBoolean(APPSEC_REPORTING_INBAND, DEFAULT_APPSEC_REPORTING_INBAND);
     appSecRulesFile = configProvider.getString(APPSEC_RULES_FILE, null);
 
     jdbcPreparedStatementClassName =
@@ -1279,6 +1284,10 @@ public class Config {
 
   public boolean isAppSecEnabled() {
     return appSecEnabled;
+  }
+
+  public boolean isAppSecReportingInband() {
+    return appSecReportingInband;
   }
 
   public String getAppSecRulesFile() {
@@ -2125,6 +2134,8 @@ public class Config {
         + configProvider
         + ", appSecEnabled="
         + appSecEnabled
+        + ", appSecReportingInband="
+        + appSecReportingInband
         + ", appSecRulesFile='"
         + appSecRulesFile
         + "'"
